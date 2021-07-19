a2b4a621927f0d8919a9d4889b7479d8d6f9018c
Mohamed Al Salti <mohamed.al.salti.96@gmail.com>
2021-07-19 08:49:35 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import gradio as gr
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+
+tokenizer = AutoTokenizer.from_pretrained("salti/arabic-t5-small-question-paraphrasing", use_fast=True)
+model = AutoModelForSeq2SeqLM.from_pretrained("salti/arabic-t5-small-question-paraphrasing").eval();
+prompt = "أعد صياغة: "
+
+@torch.inference_mode()
+def paraphrase(question):
+    question = prompt + question
+    input_ids = tokenizer(question, return_tensors="pt").input_ids
+    generated_tokens = model.generate(input_ids).squeeze().cpu().numpy()
+    return tokenizer.decode(generated_tokens, skip_special_tokens=True)
+
+  
+iface = gr.Interface(fn=paraphrase, inputs="text", outputs="text")
+iface.launch()
